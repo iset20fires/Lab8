951aac52e24989a573c93c2af049d04ddca964d5
iset20fires <cristinaveras0412@icloud.com>
2024-11-13 12:49:20 -0500

    question 2

---
+++ script.js
@@ -0,0 +1,33 @@
+// Question 1
+let firstname = "Cris";
+let age = "19";
+
+console.log (firstname);
+console.log (age);
+
+
+
+//Question 2
+
+//string:
+let name = "Cat";
+let lastname = "Dog";
+
+console.log (name);
+console.log (lastname);
+
+//number:
+let height = 11;
+let width = 8.5;
+
+console.log (height);
+console.log (width);
+
+//boolean:
+let a = true;
+let b = false;
+
+console.log (a);
+console.log (b);
+
+//undfined:
